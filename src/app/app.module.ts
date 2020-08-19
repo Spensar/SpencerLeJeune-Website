55d5794511f6678674fc494b8a914b04087e7c6a
Spensar <Spencer.Lejeune97@gmail.com>
2020-08-19 17:22:06 -0500

    Added router navigation and extra components for each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { FooterComponent } from './footer/footer.component';
+import { SoftwareComponent } from './software/software.component';
+import { YoutubeComponent } from './youtube/youtube.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'software', component: SoftwareComponent },
+  { path: 'photography', component: PhotographyComponent },
+  { path: 'youtube', component: YoutubeComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    PhotographyComponent,
+    FooterComponent,
+    SoftwareComponent,
+    YoutubeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
